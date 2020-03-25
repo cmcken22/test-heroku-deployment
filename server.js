4a8dd1e09ea019e541e64fe782ad856dc1b0e929
Conner McKenna <cmckenna@ellisdon.com>
2020-03-25 01:51:27 -0400

    testing

--- server.js
+++ server.js
@@ -34,9 +34,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   // app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 // Add routes, both API and view
 
 const initializePassport = require('./passport-config');
