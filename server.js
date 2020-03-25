7e11815703807ce8eb6f88b1cc2c99ff57c78afc
Conner McKenna <cmckenna@ellisdon.com>
2020-03-25 02:37:30 -0400

    testing

--- server.js
+++ server.js
@@ -74,10 +74,10 @@ app.get('/test2', (req, res) => {
 
 app.use('/x', routes());
 
-app.get('/*', (req, res) => {
-  console.log('GET *', path.resolve(__dirname, '../client/build/index.html'));
-  res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
-});
+// app.get('/*', (req, res) => {
+//   console.log('GET *', path.resolve(__dirname, '../client/build/index.html'));
+//   res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+// });
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mynewsnow", {
